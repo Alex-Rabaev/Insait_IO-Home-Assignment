5a7fe5147620c9de86370a21765f0a26c17d66b9
Alex-Rabaev <rabaevav@live.com>
2024-07-13 22:27:28 +0300

    update: added openai api key to config

--- backend/config.py
+++ backend/config.py
@@ -7,4 +7,5 @@ load_dotenv()
 
 class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
