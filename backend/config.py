0458fc82a80b92c0046390dcd4ce59848a906b4b
Alex-Rabaev <rabaevav@live.com>
2024-07-15 20:24:30 +0300

    add: added test database to .env, updated app Config

--- backend/config.py
+++ backend/config.py
@@ -7,5 +7,6 @@ load_dotenv()
 
 class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    TEST_SQLALCHEMY_DATABASE_URI = os.environ.get("TEST_DATABASE_URL")
     OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
