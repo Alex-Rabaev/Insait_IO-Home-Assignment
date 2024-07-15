6a4d02a291e6ddcfb04578488500fdf36cb1570b
Alex-Rabaev <rabaevav@live.com>
2024-07-15 21:16:16 +0300

    fix: removed unnecessary routes and models files

--- backend/app/models.py
+++
@@ -1,9 +0,0 @@
-from app import db
-
-
-class QuestionAnswer(db.Model):
-    __tablename__ = "questions_answers"
-
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    question = db.Column(db.Text, nullable=False)
-    answer = db.Column(db.Text, nullable=False)
